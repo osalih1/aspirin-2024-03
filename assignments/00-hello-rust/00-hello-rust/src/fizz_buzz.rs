3c9dd70b684c551d6e2d77402df2a1501294abb5
Omar Salih <osalih@0M3Rs-MBA.local>
2024-09-16 12:43:47 -0400

    Complete Assignment 0

--- assignments/00-hello-rust/00-hello-rust/src/fizz_buzz.rs
+++ assignments/00-hello-rust/00-hello-rust/src/fizz_buzz.rs
@@ -1,1 +1,17 @@
-pub fn print_fizz_buzz(max_num: u32) {}
+pub fn print_fizz_buzz(max_num: u32) {
+    for num in 1..=max_num {
+        if num % 3 == 0 && num % 5 == 0 {
+            println!("FizzBuzz");
+        } else if num % 3 == 0 {
+            println!("Fizz");
+        } else if num % 5 == 0 {
+            println!("Buzz");
+        } else {
+            println!("{}", num);
+        }
+    }
+}
+
+fn main() {
+    print_fizz_buzz(15);
+}
